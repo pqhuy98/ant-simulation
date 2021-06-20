8301d7607ff1fb0513c5cf5e89a9f49ba3520385
Huy Pham <huypham060398@gmail.com>
2021-06-20 22:52:28 +0200

    feat: remove /game

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,47 +1,9 @@
 import React from "react"
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link,
-} from "react-router-dom"
 import GameView from "./components/GameView"
-
-function Home() {
-    return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-                <li>
-                    <Link to="/game">Game</Link>
-                </li>
-            </ul>
-        </nav>
-    )
-}
 
 function App() {
     return (
-        <Router>
-            <div>
-                <Switch>
-                    <Route path="/about">
-                        <h1> About </h1>
-                    </Route>
-                    <Route path="/game">
-                        <GameView />
-                    </Route>
-                    <Route path="/">
-                        <Home />
-                    </Route>
-                </Switch>
-            </div>
-        </Router>
+        <GameView />
     )
 }
 
